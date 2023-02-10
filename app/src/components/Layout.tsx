67b19b2a9075872b4150f4027749737510367f01
yiwmsh <lonelevelsands@gmail.com>
2023-02-10 16:57:58 -0700

    🚧

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,9 +1,14 @@
+import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
+
+const Navbar = styled.nav`
+  width: 100vw;
+`;
 
 export const Layout: React.FC = () => {
   return (
     <div>
-      <p>Layout!</p>
+      <Navbar></Navbar>
       <Outlet />
     </div>
   );
--- app/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-export const Home: React.FC = () => {
-  return (
-    <body>
-      <p>Homepage!</p>
-    </body>
-  );
-};
