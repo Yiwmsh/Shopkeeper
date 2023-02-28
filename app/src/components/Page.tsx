12d469c05d08e21fc0ed2b2b880537ff77d4ff6e
yiwmsh <lonelevelsands@gmail.com>
2023-02-27 18:19:14 -0700

    🚧

---
+++ app/src/components/Page.tsx
@@ -0,0 +1,8 @@
+import { SemanticColors } from '@chrisellis/react-carpentry';
+import styled from '@emotion/styled';
+
+export const Page = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: var(${SemanticColors.background});
+`;
---
+++ itemHandler.js
@@ -0,0 +1,38 @@
+const { ipcMain } = require('electron/main');
+const fs = require('fs');
+
+// const itemSavePath = 'C:/Program Files/Shopkeeper/assets/items.txt';
+const itemSavePath = './assets/items.txt';
+
+ipcMain.on('saveItems', async (e, opt) => {
+  if (opt.items) {
+    try {
+      const items = opt.items;
+      fs.writeFile(itemSavePath, JSON.stringify(items), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    console.log('Nothing doing.');
+  }
+});
+
+ipcMain.handle('loadItems', async (e, opt) => {
+  return await loadItems();
+});
+
+const loadItems = async () => {
+  let result = [];
+
+  try {
+    const data = fs.readFileSync(itemSavePath, { encoding: 'utf-8' });
+    result = eval(data);
+  } catch (e) {
+    result = e;
+  }
+  return result;
+};
