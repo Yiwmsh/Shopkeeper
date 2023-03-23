c15af102eb1e033cff1a165a3ccd1d52e0c14b8c
yiwmsh <lonelevelsands@gmail.com>
2023-03-22 19:48:53 -0600

    ✨ Shops Search

--- app/src/pages/shops/ShopsList/ShopsList.tsx
+++ app/src/pages/shops/ShopsList/ShopsList.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
+import React from 'react';
 import { Button } from '../../../components';
 import { Shop } from '../../../types/shop';
+import { Input } from '../../items';
 
 const ShopsListContainer = styled.div`
   padding: 20px;
@@ -16,11 +18,31 @@ export const ShopsList: React.FC<{
   shops: Shop[];
   setSelectedShop: (selectedShop: Shop) => void;
 }> = ({ shops, setSelectedShop }) => {
+  const [filter, setFilter] = React.useState('');
+  const [filteredShops, setFilteredShops] = React.useState<Shop[]>(shops);
   return (
-    <ShopsListContainer>
-      {shops.map((shop) => (
-        <Button onClick={() => setSelectedShop(shop)}>{shop.name}</Button>
-      ))}
-    </ShopsListContainer>
+    <>
+      <Input
+        value={filter}
+        label="Search"
+        onChange={(value) => {
+          setFilter(value);
+          if (value === '') {
+            setFilteredShops(shops);
+          } else {
+            setFilteredShops(
+              filteredShops.filter((shop) =>
+                shop.name.toLowerCase().includes(value.toLowerCase())
+              )
+            );
+          }
+        }}
+      />
+      <ShopsListContainer>
+        {filteredShops.map((shop) => (
+          <Button onClick={() => setSelectedShop(shop)}>{shop.name}</Button>
+        ))}
+      </ShopsListContainer>
+    </>
   );
 };
